e8c51fea5d957ee160e5476ad1ce8130b3cf7487
ramini1996 <raminisriharsha1996@gmail.com>
2017-10-21 17:16:54 +0530

    Commit Speak Extension

---
+++ Speak/eventPage.js
@@ -0,0 +1,11 @@
+var menuItem={
+	"id":"speak",
+	"title":"Speak It",
+	"contexts":["selection"]
+};
+chrome.contextMenus.create(menuItem);
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if(clickData.menuItemId=="speak" && clickData.selectionText){
+		chrome.tts.speak(clickData.selectionText,{'rate':1.0});
+	}
+});
